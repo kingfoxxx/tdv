5051cccb9c2c81c215ecbc409cde4b26e0219418
RIKIN BRIGHT <122529361+kingfoxxx@users.noreply.github.com>
2023-03-30 12:31:35 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -90,20 +90,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': config(
-#         'DATABASE_URL',
-#         default='',
-#         cast=db_url
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': config(
+        'DATABASE_URL',
+        default='',
+        cast=db_url
+    )
+}
 
 
 # Password validation
